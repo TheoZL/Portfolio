3dd5abf605403d0387b2a842a555a1dbb181606e
TheoZL <zipteo@hotmail.com>
2024-10-08 20:22:55 -0600

    Add project files

---
+++ Theo_Portfolio/main.js
@@ -0,0 +1,32 @@
+function pointer(e){
+  const ejeX = e.clientX;
+  const ejeY = e.clientY + window.scrollY;
+
+  console.log(`x= ${ejeX} y= ${ejeY}`)
+  document.getElementById("pointer").style.background=`
+              radial-gradient(
+              600px at ${ejeX}px ${ejeY}px,
+              rgba(35, 169, 146, 0.25),
+              transparent 80%
+            )`
+}
+
+document.addEventListener('mousemove',pointer);
+
+// Main image with description 
+const featuredImage = document.getElementById('featured-image');
+const imageDescription = document.getElementById('image-description');
+
+// all little images
+const smallImages = document.querySelectorAll('.small-image');
+
+
+smallImages.forEach(image => {
+    image.addEventListener('click', () => {
+        // change the image
+        featuredImage.src = image.src;
+        
+        // change the descriotion acording the image selected
+        imageDescription.textContent = image.getAttribute('data-description');
+    });
+});
---
+++ Theo_Portfolio/tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        silver: '#c0c0c0',
+        silverGold: '#c8b89b',
+        paleGold: '#d1a779',
+        mediumGold: '#d9a052',
+        richGold: '#e0a032',
+        goldenYellow: '#e8a000',
+        deepGolden: '#f0a020',
+        darkGold: '#f8a840',
+        bronzeGold: '#ffb060',
+        'base-green': {
+        '50': '#f1fcf8',
+        '100': '#d0f7ec',
+        '200': '#a1eeda',
+        '300': '#6adec3',
+        '400': '#3cc5ab',
+        '500': '#23a992',
+        '600': '#198876',
+        '700': '#186d60',
+        '800': '#18574f',
+        '900': '#16423c',
+        '950': '#082b28',
+    },
+    
+      },
+    },
+  },
+  plugins: [],
+}
+
