1117ed12e6a5fada79da773fb23ac17bf75499f0
TheoZL <zipteo@hotmail.com>
2024-10-08 22:36:13 -0600

    JS fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,17 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig ({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        input: './index.html',
+        entryFileNames: '[name].js',
+      }
+    }
+  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+})
